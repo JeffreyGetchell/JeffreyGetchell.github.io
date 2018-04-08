10d3c3da9951c1b9adcfba835bcf101557197558
Jeffrey Getchell <getchell.jeffrey@gmail.com>
2018-04-08 13:30:45 -0400

    Add files via upload

--- beastlord_dist_scrolled/js/main.js
+++ beastlord_dist_scrolled/js/main.js
@@ -10,14 +10,23 @@ $(window).on('scroll', function () {
     $('img').addClass('smallerlogo');
 		$('html').css('border-color','#BF2D13').css('transition', 'all 2.5s ease');
 		$('p').css('color', '#D6411C').css('transition', 'all 1s ease');
+		$('h1').velocity({ opacity: .5 }, 2500);
+		// $('h1').velocity({ height: '2em' }, { loop: 4 });
+    $('p').velocity({ width: '600px' }, 50 );
   }  else {
     $('img').removeClass('smallerlogo');
 		$('html').css('border-color','#242626');
 		$('p').css('color', '#990803');
   }
 
+
 });
 
 
 
-// 400fo6
+
+
+
+
+
+// <<..........bottom............>>
